5bbf4b52caa2fdfe1daf239ba735e1b080a559f4
Nick Heacock <nickh84@gmail.com>
2012-09-17 00:09:38 -0400

    Updated MainWindow::about()

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -195,8 +195,23 @@ void MainWindow::cvideoCam()
 void MainWindow::about()
 {
     QMessageBox::about(this, tr("About Application"),
-                       tr("Download a Webcam Image over the internet at a timed interval. \n\n"
-                          VERSION));
+                       QString("Webcam Downloader " VERSION
+                          "\n\n"
+                          "Built with Qt " QT_VERSION_STR " (using Qt %1)"
+                          "\n\n"
+                          "Copyright (C) 2012 Nick Heacock.\n"
+                          "This program is free software: you can redistribute it and/or modify\n"
+                          "it under the terms of the GNU General Public License as published by\n"
+                          "the Free Software Foundation, either version 3 of the License, or\n"
+                          "(at your option) any later version.\n"
+                          "\n"
+                          "This program is distributed in the hope that it will be useful,\n"
+                          "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                          "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                          "GNU General Public License for more details.\n"
+                          "\n"
+                          "You should have received a copy of the GNU General Public License\n"
+                          "along with this program.  If not, see <http://www.gnu.org/licenses/>.").arg(qVersion()));
 }
 
 void MainWindow::readSettings()
